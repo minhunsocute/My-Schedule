7ac13c2ff799d7e349207f642b6cd6fd3d28a88a
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2022-10-14 23:00:28 +0700

    UI ww

--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
-console.log("Hello");
-
-// run sever
-
+//IMPORTS FROM PACKAGES
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+// IMPORTS FROM OTHER FILE 
+const authRouter = require();
+
 const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://MinhHung:20112002@cluster0.9kjcygv.mongodb.net/?retryWrites=true&w=majority";
 
 //CREATING AN API
-app.get('/hello-world', (req, res) => {
-    res.send("Hello world");
-});
+
+// app.get('/', (req, res) => {
+//     res.json({ 'name': 'Minh Hung' });
+// })
+
+// app.get('/hello-world', (req, res) => {
+//     // res.send("Hello world");
+//     res.json({ hi: "Hello World" });
+// });
 // GET, PUT, POST, DELETE, UPDATE->CRUD
 
-app.listen(PORT, "0.0.0.0", () => {
+mongoose.connect(DB).then(() => {
+        console.log("Connection Successful");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+app.listen(PORT, () => { // if remove 0.0.0.0 can use local host
     console.log(`connect at port ${PORT} hello`);
 });
---
+++ server/routes/auth.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const authRouter = express.Router();
+
+authRouter.get('/user', (req, res) => {
+    res.json({ msg: "Rivann" });
+});
